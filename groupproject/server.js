ffa0f9873e91b4167666c8872880954f211cefcf
kimura-25 <EMAIL>
2021-04-26 12:07:52 -1000

    4.26.21
    figured out how to do student table and connect value to single

--- groupproject/server.js
+++ groupproject/server.js
@@ -87,8 +87,6 @@ app.post("/submitapp", function (request, response) {
             response_form += `<tr><td> ${res_json[i].Advising_date}</td>`;
             response_form += `<td> ${res_json[i].Advising_note}</td>`;
           }
-        }
-        
  studentinfo=+`<table cellpadding="10" border="1">
           <tr>
               <th>Student ID</th>
@@ -115,13 +113,88 @@ app.post("/submitapp", function (request, response) {
   </body>
   </html>`;
   response.send(studentinfo);
-}
+        }*/
+
+function studentinformation(POST, response){
+          //note to self: need to create cases if person has no advising notes
+          var sql = "SELECT * FROM student, student_major WHERE s_id = st_id"; //query for the given student
+          con.query(sql,function (err, result, fields){ //run the query
+          if (err) throw err;
+        //  console.log(result);
+          var res_string = JSON.stringify(result);
+          var res_json = JSON.parse(res_string);
+          console.log(res_json);
+        
+          //now build the response for student detail page
+          response_form=`<!DOCTYPE html>
+          <html lang="en">
+          <head>
+              <meta charset="UTF-8">
+              <meta http-equiv="X-UA-Compatible" content="IE=edge">
+              <meta name="viewport" content="width=device-width, initial-scale=1.0">
+              <link rel="stylesheet" href="style.css">
+              <title>Student Notes</title>
+          </head>
+          <body>
+              <h1>Shidler Career Services and Professional Development</h1> 
+              <h2>Student Notes</h2>
+          
+             <!-- The navigation menu -->
+              <div class="navbar">
+                  <a href="index.html">Home</a>
+                  <div class="subnav">
+              
+                    <button class="subnavbtn">Companies<i class="fa fa-caret-down"></i></button>
+                    <div class="subnav-content">
+                      <a href="./employers.html">Employers</a>
+                      <a href="./contacts.html">Contacts</a>
+                      <a href="./jobpostings.html">Job Postings</a>
+                    </div>
+                  </div>
+              
+                  <div class="subnav">
+                    <button class="subnavbtn">Events<i class="fa fa-caret-down"></i></button>
+                    <div class="subnav-content">
+                      <a href="./careerexpo.html">Career Expo</a>
+                      <a href="./addemployer.html">Add Employer</a>
+                    </div>
+                  </div>
+              
+                  <div class="subnav">
+                    <button class="subnavbtn">Students<i class="fa fa-caret-down"></i></button>
+                    <div class="subnav-content">
+                      <a href="./studentinformation.html">Student Information</a>
+                      <a href="./advising.html">Advising</a>
+                
+                    </div>
+                  </div>
+                </div>
+                <br>
+                <br>`
+              response_form += `<table border="3" cellpadding="5" cellspacing="5">`;
+              response_form += `<td><B>Name</td><td><B>Phone</td><td><B>Email</td><td><B>Major</td></b>`;
+              for (i in res_json){
+                console.log(i)
+                response_form +=`<form action="/advisingnotes" method="POST">`;
+                response_form +=`
+                <tr>
+                <td>${res_json[i].S_fname} ${res_json[i].S_lname}
+                <input type="hidden" id="s_id" name="s_id" value="${res_json[i].S_id}">
+                <input type="submit" name="${res_json[i].S_id}" value="View"></form></td>
+                <td>${res_json[i].S_phone}</td>
+                <td>${res_json[i].S_email}</td>
+                <td>${res_json[i].St_major}</td>`
+                }
+              response_form += `</tr></html>`;
+              response.send(response_form);
+        });
+        }
 
 app.get("/studentinformation", function (request, response) {
   let POST = request.body;
   studentinformation(POST, response);
 });
-*/
+
 
 function submitapp(POST, response){
   //now build the response for student detail page
@@ -173,9 +246,10 @@ app.post("/submitapp", function (request, response) {
 });
 
 //note:need to change attributes to eds
-function query_advisingnote(POST, response){
+/*function query_advisingnote(request, response){
   //note to self: need to create cases if person has no advising notes
-  var sql = "SELECT * FROM student, student_major WHERE s_id = st_id AND s_id = '801'"; //query for the given student
+  s_id = request.query.s_id;
+  var sql = "SELECT * FROM student, student_major WHERE s_id = st_id AND s_id = " + s_id; //query for the given student
 //  var sql1 = "SELECT IF(EXISTS(SELECT * FROM advises WHERE stud_id = '801'), '1','0'"; //query for the given student
 //  console.log(sql1);
   con.query(sql,function (err, result, fields){ //run the query
@@ -245,11 +319,11 @@ function query_advisingnote(POST, response){
         response_form += `<td> ${res_json[i].Advising_note}</td>`;
       }
       response_form += "</table>";
-      response_form += `<input type="submit" value="Add Advising Note"> </form>`;*/
+      response_form += `<input type="submit" value="Add Advising Note"> </form>`;
       response_form += `</html>`;
       response.send(response_form);
 });
-}
+}*/
 
 
 
@@ -285,9 +359,89 @@ app.post("/process_jobsearch", function (request,response){
 
 
 app.post("/advisingnotes", function (request, response) {
-  let POST = request.body;
-  query_advisingnote(POST, response);
+    //note to self: need to create cases if person has no advising notes
+    s_id = request.body.s_id;
+    console.log(s_id);
+    advisingnote(request,response);
 });
+
+function advisingnote(request,response){
+  s_id = request.body.s_id;
+  var sql = "SELECT * FROM student, student_major WHERE s_id = st_id AND s_id = " + s_id; //query for the given student
+  //  var sql1 = "SELECT IF(EXISTS(SELECT * FROM advises WHERE stud_id = '801'), '1','0'"; //query for the given student
+  //  console.log(sql1);
+    con.query(sql,function (err, result, fields){ //run the query
+    if (err) throw err;
+  //  console.log(result);
+    var res_string = JSON.stringify(result);
+    var res_json = JSON.parse(res_string);
+    console.log(res_json);
+  
+    //now build the response for student detail page
+    response_form=`<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style.css">
+        <title>Student Notes</title>
+    </head>
+    <body>
+        <h1>Shidler Career Services and Professional Development</h1> 
+        <h2>Student Notes</h2>
+    
+       <!-- The navigation menu -->
+        <div class="navbar">
+            <a href="index.html">Home</a>
+            <div class="subnav">
+        
+              <button class="subnavbtn">Companies<i class="fa fa-caret-down"></i></button>
+              <div class="subnav-content">
+                <a href="./employers.html">Employers</a>
+                <a href="./contacts.html">Contacts</a>
+                <a href="./jobpostings.html">Job Postings</a>
+              </div>
+            </div>
+        
+            <div class="subnav">
+              <button class="subnavbtn">Events<i class="fa fa-caret-down"></i></button>
+              <div class="subnav-content">
+                <a href="./careerexpo.html">Career Expo</a>
+                <a href="./addemployer.html">Add Employer</a>
+              </div>
+            </div>
+        
+            <div class="subnav">
+              <button class="subnavbtn">Students<i class="fa fa-caret-down"></i></button>
+              <div class="subnav-content">
+                <a href="./studentinformation.html">Student Information</a>
+                <a href="./advising.html">Advising</a>
+          
+              </div>
+            </div>
+          </div>
+          <br>
+          <br>`
+      for (i in res_json){
+    response_form +=`<p>Name: ${res_json[i].S_fname} ${res_json[i].S_lname}</p>
+    <p>Phone: ${res_json[i].S_phone}</p>
+    <p>Email: ${res_json[i].S_email}</p>
+    <p>Major: ${res_json[i].St_major}</p>`
+                    }
+  /*  response_form += `<form action="advising.html" method="GET">`;
+    response_form += `<table border="3" cellpadding="5" cellspacing="5" bgcolor="white">`;
+    response_form += `<td><B>Advising Note</td><td><B>Date</td></b>`;
+        for (i in res_json) {
+          response_form += `<tr><td> ${res_json[i].Advising_date}</td>`;
+          response_form += `<td> ${res_json[i].Advising_note}</td>`;
+        }
+        response_form += "</table>";
+        response_form += `<input type="submit" value="Add Advising Note"> </form>`;*/
+        response_form += `</html>`;
+        response.send(response_form);
+  });
+}
 
 
 function query_employers(POST, response) {
